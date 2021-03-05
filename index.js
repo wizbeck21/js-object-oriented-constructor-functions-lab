4284e7ab3d6561098cffa17ca4b43e03ce4d77e2
Wesley <wes.beck21@outlook.com>
2021-03-04 18:16:00 -0800

    commit tree f8ce1d8b5bbe05cd18877b8f8f9efb796da2e986

---
+++ index.js
@@ -0,0 +1,18 @@
+function Scooter(year, color, model){
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+class Driver {
+  constructor(name, age, experience) {
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+}
+
+function PickupLocation(address, city){
+  this.address = address;
+  this.city = city;
+}
